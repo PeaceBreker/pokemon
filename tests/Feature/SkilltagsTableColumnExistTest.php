defe220b97d6ed924c6ff2868d60ec4ec4296b62
Wu Patrick <wupatrick@WudeMacBook-Air.local>
2023-10-26 17:07:04 +0800

    feat:SkilltagsTableColumnExistTest

---
+++ tests/Feature/SkilltagsTableColumnExistTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class SkilltagsTableColumnExistTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_skilltags_column_exist(): void
+    {
+        $this->assertTrue(Schema::hasColumn('skilltags', 'race_id'));
+        $this->assertTrue(Schema::hasColumn('skilltags', 'skill_id'));
+    }
+}
