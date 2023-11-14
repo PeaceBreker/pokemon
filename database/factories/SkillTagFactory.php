f6758d737929ebab6b19e3441e644455b1200204
Wu Patrick <wupatrick@WudeMacBook-Air.local>
2023-11-14 15:01:11 +0800

    fix:SkilltagFactory

--- database/factories/SkillTagFactory.php
+++ database/factories/SkillTagFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SkillTag>
  */
-class SkillTagFactory extends Factory
+class SkilltagFactory extends Factory
 {
     /**
      * Define the model's default state.
