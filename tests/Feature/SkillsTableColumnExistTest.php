1ebbed9cc502bb38130ccd4a04ce70d8b888ba4c
Wu Patrick <wupatrick@WudeMacBook-Air.local>
2023-11-15 15:35:40 +0800

    feat:新增測試功能SkillsTableColumnExistTest

---
+++ tests/Feature/SkillsTableColumnExistTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class SkillsTableColumnExistTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_skills_column_exist(): void
+    {
+        $this->assertTrue(Schema::hasColumn('skills', 'name'));
+    }
+}
