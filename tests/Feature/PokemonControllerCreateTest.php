5d6552b06c4234e1e094fc5d432d009f41f100c6
Wu Patrick <wupatrick@WudeMacBook-Air.local>
2023-11-15 15:35:40 +0800

    feat:新增測試功能PokemonShowTest feat:新增功能PokemonFactory refactor:修正PokemonControllerCreateTest刪除不必要變數

--- tests/Feature/PokemonControllerCreateTest.php
+++ tests/Feature/PokemonControllerCreateTest.php
@@ -16,23 +16,23 @@ class PokemonControllerCreateTest extends TestCase
     use RefreshDatabase;
 
     public function test_example(): void
-    {   
+    {
         // 基礎假資料生成
-        $races = Race::factory()
-                ->count(3)
-                ->sequence(
-                    ['name' => 'Bulbasaur'],
-                    ['name' => 'Ivysaur'],
-                    ['name' => 'Venusaur'],
-                )
-                ->create();
-        $ability = Ability::factory()->count(5)->create();
-        $nature = Nature::factory()->count(5)->create();
-        $skill = Skill::factory()->count(5)->create();
+        Race::factory()
+            ->count(3)
+            ->sequence(
+                ['name' => 'Bulbasaur'],
+                ['name' => 'Ivysaur'],
+                ['name' => 'Venusaur'],
+            )
+            ->create();
+        Ability::factory()->count(5)->create();
+        Nature::factory()->count(5)->create();
+        Skill::factory()->count(5)->create();
         // 模擬skilltags假資料
-        $skilltagForRace1 = Skilltag::factory()->count(4)->forRaceId(1)->create();
-        $skilltagForRace2 = Skilltag::factory()->count(4)->forRaceId(2)->create();
-        $skilltagForRace3 = Skilltag::factory()->count(4)->forRaceId(3)->create();
+        Skilltag::factory()->count(4)->forRaceId(1)->create();
+        Skilltag::factory()->count(4)->forRaceId(2)->create();
+        Skilltag::factory()->count(4)->forRaceId(3)->create();
         // 模拟用户输入数据
         $pokemonData = [
             'name' => 'Luka',
@@ -40,7 +40,7 @@ class PokemonControllerCreateTest extends TestCase
             'nature_id' => 1,
             'race_id' => 1,
             'ability_id' => 1,
-            'skill' => [1,2,3,4],
+            'skill' => [1, 2, 3, 4],
         ];
         // 发送 POST 请求到控制器的 API 端点
         $response = $this->post('/api/pokemons', $pokemonData);
