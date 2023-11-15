003b132a34fa5abec6d863bc101840d053a897bd
Wu Patrick <wupatrick@WudeMacBook-Air.local>
2023-11-15 15:35:40 +0800

    feat:新增SkillLogic功能 refactor(pokemoncontroller):簡化控制器邏輯至服務 fix:修正PokemonService邏輯 refactor(learnskilllogictest):改寫邏輯 chore:刪除PokemonEvolutionLogicTest refactor(composer.json):改寫autoload邏輯

--- tests/Unit/LearnSkillLogicTest.php
+++ tests/Unit/LearnSkillLogicTest.php
@@ -8,25 +8,16 @@ use PHPUnit\Framework\TestCase;
 class LearnSkillLogicTest extends TestCase
 {
     use DatabaseTransactions;
-    /**
-     * A basic unit test example.
-     */
+    
     public function testUserCanLearnSkill(): void
     {
-        // 模擬skilltags假資料
-        $skilltags = [
-            ['race_id' => 1, 'skill_id' => 1],
-            ['race_id' => 1, 'skill_id' => 2],
-            ['race_id' => 1, 'skill_id' => 3],
-            ['race_id' => 1, 'skill_id' => 4],
-        ];
+        // 模擬skillTags假資料
+         $skillTags = [1, 2, 3, 4];
         // 模拟用户输入数据
-        $pokemonData = ['skill' => [1, 2, 3, 5],];
+        $pokemonData = [1, 2, 3, 5];
         // foreach循環查看檢查是否在array裡，這裡是模擬錯誤的消息，以驗證邏輯正確性
-        foreach ($pokemonData['skill'] as $skills) {
-            if (!in_array($skills, $skilltags)) {
-                $this->assertTrue(True ,'Pokémon cannot learn these skills');
-            }
-        }
+
+        $this->assertEquals(false, SkillLogic($pokemonData, $skillTags));
+    
     }
 }
