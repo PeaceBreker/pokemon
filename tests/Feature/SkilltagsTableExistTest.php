7217b955cccba8262be89dd75604dfc231cd004a
Wu Patrick <wupatrick@WudeMacBook-Air.local>
2023-11-15 15:35:40 +0800

    feat:新增測試功能SkilltagsTableExistTest

---
+++ tests/Feature/SkilltagsTableExistTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class SkilltagsTableExistTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_skilltags_table_exist(): void
+    {
+        // 運行迁移
+       Artisan::call('migrate');
+    
+       // 斷言數據庫中是否有預期的表
+       $this->assertTrue(Schema::hasTable('skilltags'));
+    }
+}
