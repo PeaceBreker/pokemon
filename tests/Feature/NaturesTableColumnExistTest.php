8ef5b4013773c712c09e2d7604885d7d0c10bbec
Wu Patrick <wupatrick@WudeMacBook-Air.local>
2023-11-15 15:35:40 +0800

    feat:新增測試功能NaturesTableColumnExistTest

---
+++ tests/Feature/NaturesTableColumnExistTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class NaturesTableColumnExistTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_natures_column_exist(): void
+    {
+        $this->assertTrue(Schema::hasColumn('natures', 'name'));
+    }
+}
